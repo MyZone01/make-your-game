7d96828800eec544dfa47acc8d4d4595012bc8f4
serignmbaye <barsaliou1998@gmail.com>
2023-11-05 06:04:04 +0000

    feat: player movement taking into account the map

--- bomber-man/js/player.js
+++ bomber-man/js/player.js
@@ -1,50 +1,45 @@
+import { board } from "./board.js"
+
 export const player = document.createElement('div')
-export const playerPosition = { x: 11, y: 11 }
+export const playerPosition = { x: 2, y: 2 }
 player.style.gridRowStart = playerPosition.y
 player.style.gridColumnStart = playerPosition.x
 player.classList.add('player')
 let inputDirection = { x: 0, y: 0 }
-let lastInputDirection = { x: 0, y: 0 }
 
 window.addEventListener('keydown', e => {
   switch (e.key) {
     case 'ArrowUp':
-      // if (lastInputDirection.y !== 0) break
       inputDirection = { x: 0, y: -1 }
-      playerPosition.x += inputDirection.x
-      playerPosition.y += inputDirection.y
       break
     case 'ArrowDown':
-      // if (lastInputDirection.y !== 0) break
       inputDirection = { x: 0, y: 1 }
-      playerPosition.x += inputDirection.x
-      playerPosition.y += inputDirection.y
       break
     case 'ArrowLeft':
-      // if (lastInputDirection.x !== 0) break
       inputDirection = { x: -1, y: 0 }
-      playerPosition.x += inputDirection.x
-      playerPosition.y += inputDirection.y
       break
     case 'ArrowRight':
-      // if (lastInputDirection.x !== 0) break
       inputDirection = { x: 1, y: 0 }
-      playerPosition.x += inputDirection.x
-      playerPosition.y += inputDirection.y
       break
   }
 })
 
-export function getInputDirection() {
-  lastInputDirection = inputDirection
-  return inputDirection
-}
-
 export function update() {
-  const inputDirection = getInputDirection()
+  const newPositionX = playerPosition.x + inputDirection.x
+  const newPositionY = playerPosition.y + inputDirection.y
+  inputDirection = { x: 0, y: 0 }
 
-  player.style.gridRowStart = playerPosition.y
-  player.style.gridColumnStart = playerPosition.x
+  if (board[newPositionY - 1][newPositionX - 1] === 'V') {
+    playerPosition.x = newPositionX
+    playerPosition.y = newPositionY
 
-  // console.log(playerPosition);
+    player.style.gridRowStart = playerPosition.y
+    player.style.gridColumnStart = playerPosition.x
+
+  }
+  // else {
+  //   console.log(board[newPositionY - 1][newPositionX - 1]);
+  //   console.log(`c-${newPositionY}-${newPositionX}`);
+  //   console.log(document.getElementById(`c-${newPositionY}-${newPositionX}`));
+  // }
 }
