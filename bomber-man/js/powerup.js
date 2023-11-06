6b404d2ece710d6a71d009db1305e46a723ee864
serignmbaye <barsaliou1998@gmail.com>
2023-11-06 14:39:27 +0000

    fix: cancel previous power up timer when a new is got

--- bomber-man/js/powerup.js
+++ bomber-man/js/powerup.js
@@ -1,6 +1,5 @@
 export class PowerUp {
     constructor(type) {
-        this.id = Date.now();
         this.type = type;
         this.duration = 15 * 1000; // in seconds
     }
@@ -8,22 +7,27 @@ export class PowerUp {
     applyEffect(game) {
         if (this.type === 'X') {
             game.increaseBombCount();
-            game.timerManager.addTimer(this.id, () => {
+            game.timerManager.cancelTimer(this.type);
+            game.timerManager.addTimer(this.type, () => {
                 game.resetBombCount();
             }, this.duration);
-            game.timerManager.startTimer(this.id);
+            game.timerManager.startTimer(this.type);
         } else if (this.type === 'S') {
             game.changeBombType('super');
-            game.timerManager.addTimer(this.id, () => {
+            game.timerManager.cancelTimer(this.type);
+            game.timerManager.cancelTimer("M");
+            game.timerManager.addTimer(this.type, () => {
                 game.changeBombType('simple');
             }, this.duration);
-            game.timerManager.startTimer(this.id);
+            game.timerManager.startTimer(this.type);
         } else if (this.type === 'M') {
             game.changeBombType('manual');
-            game.timerManager.addTimer(this.id, () => {
+            game.timerManager.cancelTimer(this.type);
+            game.timerManager.cancelTimer("S");
+            game.timerManager.addTimer(this.type, () => {
                 game.changeBombType('simple');
             }, this.duration);
-            game.timerManager.startTimer(this.id);
+            game.timerManager.startTimer(this.type);
         }
     }
 }
--- bug-reports.md
+++ bug-reports.md
@@ -1,2 +1,2 @@
-+   [ ] when a power up is the last timer must be reset
++   [x] when a power up is the last timer must be reset
 +   [ ] prevent player to place bomb on top of power up
